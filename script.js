eba23ead64e58cb0d0ec43938b5695b7d9e08ed7
Sarunas Cironka <cironka.s@gmail.com>
2024-05-21 10:52:19 +0300

    added favicon

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const playBtn = document.querySelector(".playBtn");
-  const pauseBtn = document.querySelector(".pauseBtn");
-  const stopBtn = document.querySelector(".stopBtn");
-  const songSelect = document.getElementById("songSelect");
-  const audioPlayer = document.getElementById("audioPlayer");
-  const currentSongTitle = document.querySelector(".currentSong h3");
-  const volumeRange = document.getElementById("volumeRange");
+const playBtn = document.querySelector(".playBtn");
+const pauseBtn = document.querySelector(".pauseBtn");
+const stopBtn = document.querySelector(".stopBtn");
+const songSelect = document.getElementById("songSelect");
+const audioPlayer = document.getElementById("audioPlayer");
+const currentSongTitle = document.querySelector(".currentSong h3");
+const volumeRange = document.getElementById("volumeRange");
 
-  const loadSong = (song) => {
-    audioPlayer.src = `assets/${song}`;
-    currentSongTitle.textContent =
-      songSelect.options[songSelect.selectedIndex].text;
-  };
+const loadSong = (song) => {
+  audioPlayer.src = `assets/${song}`;
+  currentSongTitle.textContent =
+    songSelect.options[songSelect.selectedIndex].text;
+};
 
+loadSong(songSelect.value);
+
+songSelect.addEventListener("change", () => {
   loadSong(songSelect.value);
+});
 
-  songSelect.addEventListener("change", () => {
-    loadSong(songSelect.value);
-  });
+playBtn.addEventListener("click", () => {
+  audioPlayer.play();
+});
 
-  playBtn.addEventListener("click", () => {
-    audioPlayer.play();
-  });
+pauseBtn.addEventListener("click", () => {
+  audioPlayer.pause();
+});
 
-  pauseBtn.addEventListener("click", () => {
-    audioPlayer.pause();
-  });
+stopBtn.addEventListener("click", () => {
+  audioPlayer.pause();
+  audioPlayer.currentTime = 0;
+});
 
-  stopBtn.addEventListener("click", () => {
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
-  });
+volumeRange.addEventListener("input", () => {
+  audioPlayer.volume = volumeRange.value;
+});
 
-  volumeRange.addEventListener("input", () => {
-    audioPlayer.volume = volumeRange.value;
-  });
-
-  audioPlayer.addEventListener("timeupdate", () => {
-    const progressBar = document.querySelector(".progress-bar");
-    const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-    progressBar.style.width = `${progress}%`;
-  });
+audioPlayer.addEventListener("timeupdate", () => {
+  const progressBar = document.querySelector(".progress-bar");
+  const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+  progressBar.style.width = `${progress}%`;
 });
